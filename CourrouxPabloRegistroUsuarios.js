7fc85d48c38f8929e20637362034639bf201f748
PabloC1981 <pablocourroux@gmail.com>
2021-11-29 23:38:37 -0300

    Implementacion jQuery en IndexRegistro

--- CourrouxPabloRegistroUsuarios.js
+++ CourrouxPabloRegistroUsuarios.js
@@ -7,32 +7,27 @@ let password1= prompt('Reitere la Contraseña');*/
 let RegistroDeClientes = [];
 //CLASE CLIENTES PARA GENERAR USUARIOS
 class Clientes {
-    constructor(name,surname,cellphone,password){
+    constructor(name,email,password){
         this.name=name
-        this.surname=surname
-        this.cellphone=cellphone
+        this.email=email
         this.password=password
     }  
 }
-function RegistrarCliente(){
-    let  name= prompt('Ingrese Su Nombre').toUpperCase();
-    let surname = prompt('Ingrese Su Apellido ').toUpperCase();
-    let cellphone = prompt('Ingrese Su Numero Celular');
-    let password = prompt('Ingrese Su Contaseña \n Debe Tenes Entre 5 y 8 Caracteres');
-    let password1= prompt('Reitere la Contraseña');    
-    
-    if(name == "" || surname == "" || isNaN(cellphone)){
-        alert("Alguno de los datos es erroneo")
-    }else if(password.length >=5 && password.length <=8 && password === password1){
+
+let check = $('#cargar');
+let btnEnviar = $('.btn');
+
+btnEnviar.click(function(){
+    let name = $('#Name');
+    let email = $('#Email');
+    let password = $('Password')
+    if($(inputName).val().length == "" && $(inputEmail).val().length == "" && $(inputPassword).val().length == ""){
         alert("Usuario Creado")
         alert("BIENVENIDOS A LAVORO \n Indumentaria Escolar")
-        RegistroDeClientes.push({name,surname,cellphone,password })
+        RegistroDeClientes.push({name,email,password})
+        console.log(RegistroDeClientes)
         }else{
-            alert("Usted no ha ingresado una contraseña valida")
-            
+            alert("Alguno de los datos es erroneo")
         }
-    }
-
-//const cliente = new Clientes ({name:name, surname:surname, cellphone:cellphone, password:password})
+    });
 console.log(RegistroDeClientes)
-RegistrarCliente();
